44305355b8724a3d88bebaa19c97e630cc85be54
energy-100 <abc551218@126.com>
2021-01-05 15:56:52 +0800

    尝试使用dropout、小型网络防止过拟合，

--- Kares_Test.py
+++ Kares_Test.py
@@ -57,6 +57,8 @@ def softmax_MultiCategoryClassification():
     model=tf.keras.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
     model.add(tf.keras.layers.Dense(128,activation='relu'))
+    model.add(tf.keras.layers.Dense(128,activation='relu'))
+    model.add(tf.keras.layers.Dense(128,activation='relu'))
     model.add(tf.keras.layers.Dense(10,activation='softmax'))
     print(model.summary())
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.01),loss='sparse_categorical_crossentropy',metrics=['acc'])#metrics=['acc']参数是指定每一步输出的信息，是列表类型
@@ -65,8 +67,44 @@ def softmax_MultiCategoryClassification():
     #两种损失函数 sparse_categorical_crossentropy
     pass
 
+# softmax多分类问题
+# 1.使用dropout防止过拟合
+# 2.添加测试数据集
+# 3.使用小型网络网址
+def softmax_dropout_MultiCategoryClassification():
+    (train_image,train_label),(test_image,test_label)=tf.keras.datasets.fashion_mnist.load_data()
+    # plt.imshow(train_image[0])
+    # plt.show()
+    train_image=train_image/255
+    test_image=test_image/255
+    model=tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+    model.add(tf.keras.layers.Dense(32,activation='relu'))
+    # model.add(tf.keras.layers.Dropout(rate=0.5))
+    model.add(tf.keras.layers.Dense(10,activation='softmax'))
+
+    # model.add(tf.keras.layers.Dense(128,activation='relu'))
+    # model.add(tf.keras.layers.Dropout(rate=0.5))
+    # model.add(tf.keras.layers.Dense(128,activation='relu'))
+    # model.add(tf.keras.layers.Dropout(rate=0.5))
+    # model.add(tf.keras.layers.Dense(128,activation='relu'))
+    # model.add(tf.keras.layers.Dropout(rate=0.5))
+    # model.add(tf.keras.layers.Dense(10,activation='softmax'))
+    print(model.summary())
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.01),loss='sparse_categorical_crossentropy',metrics=['acc'])#metrics=['acc']参数是指定每一步输出的信息，是列表类型
+    history=model.fit(train_image,train_label,epochs=20,validation_data=(test_image,test_label))
+    print(history)
+    plt.plot(history.epoch,history.history['loss'],label="loss")#训练集损失
+    plt.plot(history.epoch,history.history['val_loss'],label="val_loss")#测试集损失
+    plt.legend()
+    plt.show()
+
+    #两种损失函数 sparse_categorical_crossentropy
+    pass
+
 if __name__ == "__main__":
     # linearRegression()
     # MultilayerPercetron()
     # logisticRegression()
-    softmax_MultiCategoryClassification()
+    # softmax_MultiCategoryClassification()
+    softmax_dropout_MultiCategoryClassification()
