e81a0fdfd294e27d8d50a368a30bed89587f06f9
energy-100 <abc551218@126.com>
2021-01-08 16:02:24 +0800

    在损失函数中使用 from_logits = True 参数，网络最后不使用sigmoid 或 softmax 函数进行激活

--- CNN_Test.py
+++ CNN_Test.py
@@ -118,7 +118,7 @@ def CNN_satelliteImage_Classification():
     model.add(keras.layers.Dense(1,activation='sigmoid'))
     print(model.summary())
 
-    # # 多分类使用CategoricalCrossentropy()函数，且模型最后没有使用逻辑激活函数激活时 from_logits=False
+    # # 多分类使用CategoricalCrossentropy()函数，且模型最后没有使用逻辑激活函数(sigmoid或softmax函数)激活时 from_logits=True
     # # 标签为非 noe-hot编码时 使用 SparseCategoricalCrossentropy()函数
     # model.compile(optimizer=keras.optimizers.Adam(0.01),loss=keras.losses.CategoricalCrossentropy(from_logits=True))
     model.compile(optimizer=keras.optimizers.Adam(0.01),loss=keras.losses.Binary_crossentropy(),metrics=['acc']) # loss函数名称 要大写
@@ -130,7 +130,7 @@ def CNN_satelliteImage_Classification():
     img=load_image(img_path)
     img=tf.expand_dims(img,axis=0) # 扩展维度，由于训练时模型输入为4D batch-高-宽-通道，因此预测数据应该也为4D
     pred=model.predict(img)
-    print(index_to_label[(pred>0.5).astype('int')[0][0]])  #predict返回结果是二维结构，batch-每个输出节点数值
+    print(index_to_label[(pred>0.5).astype('int')[0][0]])  #predict返回结果是二维结构，样本数-每个输出节点数值
 
     # # 随机选取一张图片显示
     # i=random.choice(range(len(all_image_path)))
@@ -141,7 +141,146 @@ def CNN_satelliteImage_Classification():
     # plt.show()
 
 
+def CNN_birds_Classification():
+    # 读取图片函数
+    def load_image(path,label):
+        img_raw = tf.io.read_file(path)  # 二进制读取
+        img_tensor = tf.image.decode_jpeg(img_raw, channels=3)  # 解码二进制 channels 根据图片类型设定channels大小
+        img_tensor = tf.image.resize(img_tensor, [256, 256])  # 统一图片尺寸
+        img_tensor = tf.cast(img_tensor, tf.float32)  # 数据类型转换
+        # img_tensor.numpy() #转换成numpy
+        img_tensor = img_tensor / 255  # 归一化
+        return img_tensor,label
+
+    def load_preprocess_image(path,label):
+        img_raw = tf.io.read_file(path)  # 二进制读取
+        img_tensor = tf.image.decode_jpeg(img_raw, channels=3)  # 解码二进制 channels 根据图片类型设定channels大小
+        img_tensor = tf.image.resize(img_tensor, [256, 256])  # 统一图片尺寸
+        img_tensor = tf.cast(img_tensor, tf.float32)  # 数据类型转换
+        # img_tensor.numpy() #转换成numpy
+        img_tensor = img_tensor / 255  # 归一化
+        return img_tensor,label
+
+    all_image_path = glob.glob('Data/birds/*/*.jpg')
+    # print(all_image_path)
+
+    all_label_names = [v.split('\\')[1] for v in all_image_path]    #所有标签
+    label_names = set(all_label_names)  #去重后的所有标签
+    label_to_index = dict((elem,i) for i,elem in enumerate(label_names))
+    index_to_label = dict((v,k) for k,v in label_to_index.items())
+    # print([label_to_index[label] for label in all_label_names[-5:]])
+    np.random.seed(10)
+    random_index = np.random.permutation(len(all_label_names)) #讲数据乱序
+    img_path=np.array(all_image_path)[random_index]
+    label_path=np.array(all_label_names)[random_index]
+    i= int(len(all_image_path)*0.8)
+    train_path=img_path[:i]
+    train_label=label_path[:i]
+    test_path=img_path[i:]
+    test_label=label_path[i:]
+    train_ds=tf.data.Dataset.from_tensor_slices((train_path,train_label))
+    test_ds=tf.data.Dataset.from_tensor_slices((test_path,test_label))
+
+    train_ds=train_ds.map(load_image,num_parallel_calls=tf.data.experimental.AUTOTUNE)  #自动选择读取图片时使用线程数
+    test_ds=test_ds.map(load_image,num_parallel_calls=tf.data.experimental.AUTOTUNE)  #自动选择读取图片时使用线程数
+
+    BATCH_SIZE = 32
+    train_ds = train_ds.repeat().shuffle(100).batch(BATCH_SIZE)
+    test_ds = test_ds.batch(BATCH_SIZE)
+
+    random.shuffle(all_image_path)  # 乱序
+    label_to_index = {'airplane': 0, 'lake': 1}
+    index_to_label = dict((v, k) for k, v in label_to_index.items())
+    all_labels = [label_to_index[elem.split('\\')[1]] for elem in all_image_path]
+    img_ds = tf.data.Dataset.from_tensor_slices(all_image_path)  # 样本数据
+    img_ds = img_ds.map(load_image)  # 作用map()函数，将路径转化为图像数据
+    label_ds = tf.data.Dataset.from_tensor_slices(all_labels)  # 标签数据
+    img_label_ds = tf.data.Dataset.zip((img_ds, label_ds))  # 样本数据+标签数据
+
+    train_count = int(len(all_image_path) * 0.2)
+    test_count = len(all_image_path) - train_count
+    train_ds = img_label_ds.skip(test_count)
+    test_ds = img_label_ds.take(test_count)
+
+    BATCH_SIZE = 16
+    train_ds = train_ds.repeat().shuffle(100).batch(BATCH_SIZE)
+    test_ds = test_ds.batch(BATCH_SIZE)
+
+    model = keras.Sequential()
+    model.add(keras.layers.Conv2D(64, (3, 3), input_shape=(256, 256, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Conv2D(64, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.MaxPool2D())
+    model.add(keras.layers.Conv2D(128, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Conv2D(128, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.MaxPool2D())
+    model.add(keras.layers.Conv2D(256, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Conv2D(256, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.MaxPool2D())
+    model.add(keras.layers.Conv2D(512, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Conv2D(512, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.MaxPool2D())
+    model.add(keras.layers.Conv2D(512, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Conv2D(512, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Conv2D(512, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.GlobalAveragePooling2D())  # 对每层（通道）图像求平均 得到通道数量的数据数量
+    model.add(keras.layers.Dense(1024))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Dense(256))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Dense(1, activation='sigmoid'))
+    print(model.summary())
+
+    # # 多分类使用CategoricalCrossentropy()函数，且模型最后没有使用逻辑激活函数(sigmoid或softmax函数)激活时 from_logits=True，数值的变换处理会在损失函数这里处理
+    # # 标签为非 noe-hot编码时 使用 SparseCategoricalCrossentropy()函数
+    # # 再多分类问题中，一般将在最后直接输出（不使用sigmoid或softmax激活） from_logits=True 这样计算更加稳定
+    # model.compile(optimizer=keras.optimizers.Adam(0.01),loss=keras.losses.CategoricalCrossentropy(from_logits=True))
+    model.compile(optimizer=keras.optimizers.Adam(0.01), loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['acc'])  # loss函数名称 要大写
+    history = model.fit(train_ds, epoch=10, validation_data=test_ds, steps_per_epoch=len(train_path) // BATCH_SIZE,
+                        validation_steps=len(test_path)  // BATCH_SIZE)
+    print(history)
+
+    # 预测
+    img_path = 'test01.jpg'
+    img = load_preprocess_image(img_path)
+    img = tf.expand_dims(img, axis=0)  # 扩展维度，由于训练时模型输入为4D batch-高-宽-通道，因此预测数据应该也为4D
+    pred = model.predict(img)
+    print(index_to_label[np.argmax(pred)])  # predict返回结果是二维结构，样本数-每个输出节点数值
+
+    # # 随机选取一张图片显示
+    # i=random.choice(range(len(all_image_path)))
+    # image_path=all_image_path[i]
+    # tensor_image=load_image(image_path)
+    # plt.title(index_to_label[label1[i]])
+    # plt.imshow(tensor_image.numpy())
+    # plt.show()
+
 
 if __name__ == "__main__":
     # CNN_demo1()
-    CNN_satelliteImage_Classification()
+    # CNN_satelliteImage_Classification()
+    CNN_birds_Classification()
